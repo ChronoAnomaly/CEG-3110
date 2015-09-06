cf12de34d8190ca1c0668e0ee69b5c942e910bf8
ChronoAnomaly <chronoanomaly@gmail.com>
2015-09-06 16:45:48 -0400

    added project 1 and working on test script

---
+++ project1/auto_tests.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function one_dice {
+	echo 'Testing one dice output.'
+	for i in {1..10}; do
+		echo 'hello';
+	done
+}
+
+function two_dice {
+	echo 'Testing two dice output.'
+	for i in {1..10}; do
+		;
+	done
+}
+
+function three_dice {
+	echo 'Testing three dice output.'
+	for i in {1..10}; do
+		;
+	done
+}
+
+function four_dice {
+	echo 'Testing four dice output.'
+	for i in {1..10}; do
+		;
+	done
+}
+
+function five_dice {
+	echo 'Testing five dice output.'
+	for i in {1..10}; do
+		;
+	done
+}
+
+function quit {
+	echo 'Exiting testing script.'
+	exit
+}
+
+one_dice
+two_dice
+three_dice
+four_dice
+five_dice
+quit
