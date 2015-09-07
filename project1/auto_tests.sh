45d3ef95216b3f22df34446cd5a631299cdc5212
ChronoAnomaly <chronoanomaly@gmail.com>
2015-09-07 19:56:12 -0400

    changed size of test

--- project1/auto_tests.sh
+++ project1/auto_tests.sh
@@ -4,7 +4,7 @@ function one_dice {
 	echo 'Testing one dice output...'
 	for i in {1..5000}; do
 		echo '1' | ./worley_dice >> dice_1.log
-		sleep .63
+		sleep 1.6
 	done
 }
 
@@ -12,7 +12,7 @@ function two_dice {
 	echo 'Testing two dice output...'
 	for i in {1..5000}; do
 		echo '2' | ./worley_dice >> dice_2.log
-		sleep .63
+		sleep 1.6
 	done
 }
 
@@ -20,7 +20,7 @@ function three_dice {
 	echo 'Testing three dice output...'
 	for i in {1..5000}; do
 		echo '3' | ./worley_dice >> dice_3.log
-		sleep .63
+		sleep 1.6
 	done
 }
 
@@ -28,7 +28,7 @@ function four_dice {
 	echo 'Testing four dice output...'
 	for i in {1..5000}; do
 		echo '4' | ./worley_dice >> dice_4.log
-		sleep .63
+		sleep 1.6
 	done
 }
 
@@ -36,7 +36,7 @@ function five_dice {
 	echo 'Testing five dice output...'
 	for i in {1..5000}; do
 		echo '5' | ./worley_dice >> dice_5.log
-		sleep .63
+		sleep 1.6
 	done
 }
 
