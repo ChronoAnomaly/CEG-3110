2d57388aac3d1904c45cebd4473354029db532f3
ChronoAnomaly <chronoanomaly@gmail.com>
2015-09-07 19:58:20 -0400

    size

--- project1/auto_tests.sh
+++ project1/auto_tests.sh
@@ -2,7 +2,7 @@
 
 function one_dice {
 	echo 'Testing one dice output...'
-	for i in {1..5000}; do
+	for i in {1..6500}; do
 		echo '1' | ./worley_dice >> dice_1.log
 		sleep 1.6
 	done
@@ -10,7 +10,7 @@ function one_dice {
 
 function two_dice {
 	echo 'Testing two dice output...'
-	for i in {1..5000}; do
+	for i in {1..6500}; do
 		echo '2' | ./worley_dice >> dice_2.log
 		sleep 1.6
 	done
@@ -18,7 +18,7 @@ function two_dice {
 
 function three_dice {
 	echo 'Testing three dice output...'
-	for i in {1..5000}; do
+	for i in {1..6500}; do
 		echo '3' | ./worley_dice >> dice_3.log
 		sleep 1.6
 	done
@@ -26,7 +26,7 @@ function three_dice {
 
 function four_dice {
 	echo 'Testing four dice output...'
-	for i in {1..5000}; do
+	for i in {1..6500}; do
 		echo '4' | ./worley_dice >> dice_4.log
 		sleep 1.6
 	done
@@ -34,7 +34,7 @@ function four_dice {
 
 function five_dice {
 	echo 'Testing five dice output...'
-	for i in {1..5000}; do
+	for i in {1..6500}; do
 		echo '5' | ./worley_dice >> dice_5.log
 		sleep 1.6
 	done
