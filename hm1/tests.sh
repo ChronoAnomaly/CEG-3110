9d34032a5157f12f8d38857a2044186ed8ac596c
ChronoAnomaly <chronoanomaly@gmail.com>
2015-09-14 14:56:53 -0400

    updated some files and worked on test cases

--- hm1/tests.sh
+++ hm1/tests.sh
@@ -3,3 +3,11 @@
 
 echo 'Running tests.'
 
+echo 'Correct_Horse#@battery20' | ./worley_password >> log
+echo 'Do_*go13' | ./worley_password >> log
+echo 'checking_(123' | ./worley_password >> log
+echo 'HELLO((#88' | ./worley_password >> log
+echo 'CorrectBatbattery+=' | ./worley_password >> log
+echo 'RunFor&&);' | ./worley_password >> log
+echo 'ASD lpn256:_' | ./worley_password >> log
+echo 'CorrectTEST()@=754389batHouse' | ./worley_password >> log
